3c4146b7803bd674f56ec43a9273d93910ca2c8d
Shadaab-005 <shadaabali200@gmail.com>
2022-03-25 22:42:38 +0530

    firebase auth completed

--- app/src/main/java/com/example/timetablemanager/MainActivity.java
+++ app/src/main/java/com/example/timetablemanager/MainActivity.java
@@ -1,22 +1,49 @@
 package com.example.timetablemanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textview7;
     TextView forgotpassword;
+
+    EditText inputemail,inputpassword;
+    Button btnlogin;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+    ProgressDialog progressDialog;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textview7=findViewById(R.id.textview7);
         forgotpassword=findViewById(R.id.forgotpassword);
+
+        inputemail=findViewById(R.id.inputemail);
+        inputpassword=findViewById(R.id.inputpassword);
+        btnlogin=findViewById(R.id.btnlogin);
+        progressDialog= new ProgressDialog(this);
+        mAuth=FirebaseAuth.getInstance();
+        mUser=mAuth.getCurrentUser();
 
 
         textview7.setOnClickListener(new View.OnClickListener() {
@@ -31,5 +58,54 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this,forgotpassword.class));
             }
         });
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                perforLogin();
+            }
+        });
+    }
+
+    private void perforLogin() {
+        String email=inputemail.getText().toString();
+        String password=inputpassword.getText().toString();
+
+        if (!email.matches(emailPattern))
+        {
+            inputemail.setError("Enter correct Email");
+        }else if (password.isEmpty() || password.length()<6)
+        {
+            inputpassword.setError("Enter proper password");
+        }else
+        {
+            progressDialog.setMessage("Please Wait While Login....");
+            progressDialog.setTitle("Login");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+
+                        progressDialog.dismiss();
+                        sendUserToNextActivity();
+                        Toast.makeText(MainActivity.this,"Login Successful", Toast.LENGTH_SHORT).show();
+
+                    }else
+                    {
+                        progressDialog.dismiss();
+                        Toast.makeText(MainActivity.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+    }
+}
+    private void sendUserToNextActivity() {
+        Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/timetablemanager/register.java
+++ app/src/main/java/com/example/timetablemanager/register.java
@@ -1,21 +1,49 @@
 package com.example.timetablemanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class register extends AppCompatActivity {
 
     TextView alreadyhaveaccount;
+    EditText inputemail,inputpassword,inputconfirmpassword;
+    Button registerbtn;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+    ProgressDialog progressDialog;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         alreadyhaveaccount=findViewById(R.id.alreadyhaveaccount);
+
+        inputemail=findViewById(R.id.inputemail);
+        inputpassword=findViewById(R.id.inputpassword);
+        inputconfirmpassword=findViewById(R.id.inputconfirmpassword);
+        registerbtn=findViewById(R.id.registerbtn);
+        progressDialog= new ProgressDialog(this);
+        mAuth=FirebaseAuth.getInstance();
+        mUser=mAuth.getCurrentUser();
+
 
 
         alreadyhaveaccount.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +52,62 @@ public class register extends AppCompatActivity {
                 startActivity(new Intent(register.this,MainActivity.class));
             }
         });
+
+        registerbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perforAuth();
+            }
+        });
+    }
+
+    private void perforAuth() {
+        String email=inputemail.getText().toString();
+        String password=inputpassword.getText().toString();
+        String confirmpassword=inputconfirmpassword.getText().toString();
+
+        if (!email.matches(emailPattern))
+        {
+            inputemail.setError("Enter correct Email");
+        }else if (password.isEmpty() || password.length()<6)
+        {
+            inputpassword.setError("Enter proper password");
+        }else if (!password.equals(confirmpassword))
+        {
+            inputconfirmpassword.setError("Passwowd not matched");
+        }else
+        {
+            progressDialog.setMessage("Please Wait While Registration....");
+            progressDialog.setTitle("Registration");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if (task.isSuccessful())
+                    {
+                        progressDialog.dismiss();
+                        sendUserToNextActivity();
+                        Toast.makeText(register.this,"Registration Successful", Toast.LENGTH_SHORT).show();
+
+                    }else
+                    {
+                        progressDialog.dismiss();
+                        Toast.makeText(register.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        }
+
+
+    }
+
+    private void sendUserToNextActivity() {
+        Intent intent=new Intent(register.this,HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }

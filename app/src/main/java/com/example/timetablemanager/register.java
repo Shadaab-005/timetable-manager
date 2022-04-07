662ebd6f8e951b97f8ff46d5875f8193f12d9d9c
Shadaab-005 <shadaabali200@gmail.com>
2022-04-07 23:36:58 +0530

    firestore setup complete

--- app/src/main/java/com/example/timetablemanager/register.java
+++ app/src/main/java/com/example/timetablemanager/register.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,17 +14,28 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class register extends AppCompatActivity {
 
+    public static final String TAG = "register";
+
+
     TextView alreadyhaveaccount;
-    EditText inputemail,inputpassword,inputconfirmpassword;
+    EditText inputemail,inputpassword,inputconfirmpassword,inputname;
     Button registerbtn;
+    FirebaseFirestore fStore;
     String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    String userID;
 
     ProgressDialog progressDialog;
     FirebaseAuth mAuth;
@@ -36,12 +48,14 @@ public class register extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         alreadyhaveaccount=findViewById(R.id.alreadyhaveaccount);
 
+        inputname=findViewById(R.id.inputname);
         inputemail=findViewById(R.id.inputemail);
         inputpassword=findViewById(R.id.inputpassword);
         inputconfirmpassword=findViewById(R.id.inputconfirmpassword);
         registerbtn=findViewById(R.id.registerbtn);
         progressDialog= new ProgressDialog(this);
         mAuth=FirebaseAuth.getInstance();
+        fStore=FirebaseFirestore.getInstance();
         mUser=mAuth.getCurrentUser();
 
 
@@ -64,6 +78,7 @@ public class register extends AppCompatActivity {
     private void perforAuth() {
         String email=inputemail.getText().toString();
         String password=inputpassword.getText().toString();
+        String fullname=inputname.getText().toString();
         String confirmpassword=inputconfirmpassword.getText().toString();
 
         if (!email.matches(emailPattern))
@@ -91,6 +106,19 @@ public class register extends AppCompatActivity {
                         progressDialog.dismiss();
                         sendUserToNextActivity();
                         Toast.makeText(register.this,"Registration Successful", Toast.LENGTH_SHORT).show();
+                        userID=mAuth.getCurrentUser().getUid();
+                        DocumentReference documentReference=fStore.collection("Users").document();
+                        Map<String,Object> user= new HashMap<>();
+                        user.put("fName",fullname);
+                        user.put("email",email);
+                        documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Log.d(TAG, "onSuccess: user profile is created for "+ user);
+                            }
+                        });
+
+
 
                     }else
                     {
@@ -106,7 +134,7 @@ public class register extends AppCompatActivity {
     }
 
     private void sendUserToNextActivity() {
-        Intent intent=new Intent(register.this,HomeActivity.class);
+        Intent intent=new Intent(register.this,MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
